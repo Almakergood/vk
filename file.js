dd8c6828a14bdb19c5ed28615ee1c89026a13b84
Almakergood <49840366+Almakergood@users.noreply.github.com>
2024-01-31 14:00:44 +0500

    Update file.js

--- file.js
+++ file.js
@@ -2,8 +2,5 @@ window.onload = vkscript();
 
 function vkscript () 
 {
-	var vk = document.createElement('script');
-            vk.src = 'https://unpkg.com/@vkontakte/vk-bridge/dist/browser.min.js';
-            document.getElementsByTagName('head')[0].appendChild(vk);
 	    vkBridge.send('VKWebAppInit', {});
 }
